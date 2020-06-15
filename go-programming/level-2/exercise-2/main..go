e64b43f0467bd86a00774232cfc554040165eb59
nicolasgranata <nicolasegranata@gmail.com>
2020-06-15 18:42:05 -0300

    Adding level 2 exercises

---
+++ go-programming/level-2/exercise-2/main..go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := (23 == 29)
+	b := (5 <= 4)
+	c := (6 >= 9)
+	d := (19 != 19)
+	e := (30 < 1)
+	f := (19 > 0)
+	fmt.Println(a, b, c, d, e, f) 
+}
